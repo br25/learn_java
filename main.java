62d341be58b9dd7e3aad4e6b146711e155d54457
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-12-28 02:09:33 +0600

    variable

--- main.java
+++ main.java
@@ -1,7 +1,12 @@
 public class main {
     public static void main(String[] args) 
     {
-        System.out.print("Initial code");
-        System.out.println("Initial code");
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Daniel";
+
+        System.out.println("Your name is "+name);
     }
 }
