3214195ba242ac06145bdfdc6d1f72a2c5040c28
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-12-28 02:37:37 +0600

    a+b hole square

--- main.java
+++ main.java
@@ -1,12 +1,23 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) 
     {
-        String name = JOptionPane.showInputDialog("Enter your name? ");
-        JOptionPane.showMessageDialog(null, "Hello "+name);
+        double x;
+        double y;
+        double z;
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
-        JOptionPane.showMessageDialog(null, "You are "+age+" years old.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter side x: ");
+        x = scanner.nextDouble();
+        System.out.println("Enter side y: ");
+        y = scanner.nextDouble();
+
+        // root over a+b hole square
+        z = Math.sqrt((x*x)+(y*y));
+
+        System.out.println("The hypotenuse is: "+z);
+        scanner.close();
     }
 }
