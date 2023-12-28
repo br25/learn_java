7933410ecfee9ddc9158e5f854108e2b5f051640
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-12-28 14:36:15 +0600

    random data create

--- main.java
+++ main.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class main {
     public static void main(String[] args) 
     {
-        double x;
-        double y;
-        double z;
+        Random random = new Random();
 
-        Scanner scanner = new Scanner(System.in);
+        Double z = random.nextDouble();
 
-        System.out.println("Enter side x: ");
-        x = scanner.nextDouble();
-        System.out.println("Enter side y: ");
-        y = scanner.nextDouble();
-
-        // root over a+b hole square
-        z = Math.sqrt((x*x)+(y*y));
-
-        System.out.println("The hypotenuse is: "+z);
-        scanner.close();
+        System.out.println(z);
     }
 }
