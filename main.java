3aacc2ad602c2ded1404f12bf859fa4c756efc69
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-12-28 15:15:51 +0600

    if else condition

--- main.java
+++ main.java
@@ -1,12 +1,20 @@
-import java.util.Random;
-
 public class main {
     public static void main(String[] args) 
     {
-        Random random = new Random();
+        int age = 15;
 
-        Double z = random.nextDouble();
+        if (age==75) {
+            System.out.println("Boomer");
+        }
+        else if (age>=18) {
+            System.out.println("Adult");
+        }
+        else if (age>=13) {
+            System.out.println("Teeneger");
+        }
+        else {
+            System.out.println("Kids");
+        }
 
-        System.out.println(z);
     }
 }
